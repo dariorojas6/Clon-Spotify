92ad1f74c661a84adfc3e6ab8d1bfb1d9f6b0175
dariorojas6 <dario43055@gmail.com>
2023-09-29 21:54:36 -0300

    js

---
+++ main.js
@@ -0,0 +1,19 @@
+const topBar = document.querySelector('.topbar')
+
+window.addEventListener('scroll',()=>{
+    if(window.scrollY > 0){
+        topBar.classList.add('transparent')
+    }
+    else{
+        topBar.classList.remove('transparent')
+    }
+})
+
+// nueva variable para almacenar altura del topbar
+
+let topbarHeight = topBar.offsetHeight;
+
+// añadimos un padding basado en la altura al main-content o el contenido principal
+
+const mainContent = document.querySelector(".main-content")
+mainContent.style.paddingTop = `${topbarHeight + 20}px`
